2e77a5cc24feadcb1b99275c6c571d436301af91
Ratovonirina <eric.ratovonirina@gmail.com>
2024-03-08 21:52:01 +0300

    Nouvelle structure de projet pour backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
-let assignment = require('./routes/assignments');
-
+const routesAssignment = require("./routes/assignments")
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.set('debug', true);
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 const uri = 'mongodb+srv://mb1:toto@cluster0.lxvcyxy.mongodb.net/assignments?retryWrites=true&w=majority&appName=Cluster0';
+// const uri = "mongodb://localhost:27017"
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -40,19 +40,9 @@ app.use(bodyParser.json());
 // Obligatoire si déploiement dans le cloud !
 let port = process.env.PORT || 8010;
 
-// les routes
+// Les routes
 const prefix = '/api';
-
-// http://serveur..../assignments
-app.route(prefix + '/assignments')
-  .post(assignment.postAssignment)
-  .put(assignment.updateAssignment)
-  .get(assignment.getAssignments);
-
-app.route(prefix + '/assignments/:id')
-  .get(assignment.getAssignment)
-  .delete(assignment.deleteAssignment);
-
+app.use(prefix, routesAssignment)
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
