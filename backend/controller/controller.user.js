2ac9b02635b370105d377127a776300fdae4d69f
Ratovonirina <eric.ratovonirina@gmail.com>
2024-03-09 10:05:37 +0300

    Suppression unique validator : validation manuel de l'adresse email utilisateur

--- backend/controller/controller.user.js
+++ backend/controller/controller.user.js
@@ -2,19 +2,40 @@ const User = require('../model/user');
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken');
 
+// UTILISATEUR CONNECTEE
+const utilisateur_actif = (req, res) => {
+    // User.findOne()
+    if(req.auth){
+        res.json({message: req.auth.userId})
+    }else{
+        res.json({message: "Donnée vide"})
+    }
+}
+
 // INSCRIPTION UTILISATEUR
+// data : email, password, role 
 const inscription = (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            
+            if(user){
+                res.status(201).json({ message: 'Email déjà utilisé !' })
+            }else{
+                bcrypt.hash(req.body.password, 10)
+                    .then(hash => {
+                        const user = new User({
+                            email: req.body.email,
+                            role: req.body.role,
+                            password: hash
+                        });
+                        user.save()
+                            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+                            .catch(error => res.status(400).json({ error }));
+                    })
+                    .catch(error => res.status(500).json({ error }));
+            }
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(400).json({ error}))
 };
 
 // CONNEXION UTILISATEUR
@@ -51,4 +72,4 @@ const deconnexion = (req, res) => {
     res.status(200).json({ message: 'Deconnexion réussi' });
 };
 
-module.exports = { inscription, connexion, deconnexion };
+module.exports = { inscription, connexion, deconnexion, utilisateur_actif };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const user_controller = require('../controller/controller.user');
 
-// http://serveur..../user/
-router.post('utilisateur/inscription', user_controller.inscription);
-router.post('utilisateur/connexion', user_controller.connexion);
-router.post('utilisateur/deconnexion', user_controller.deconnexion);
+// http://serveur..../utilisateur/
+router.post('/utilisateur/inscription', user_controller.inscription);
+router.post('/utilisateur/connexion', user_controller.connexion);
+router.post('/utilisateur/deconnexion', user_controller.deconnexion);
 
 module.exports = router;
