53a7a43701b5acb5783360b50c98af7b096c11fb
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2024-04-18 20:47:50 +0300

    [LOGIN] Amliorer page login #main

--- frontend/src/app/pages/user/login/login.component.ts
+++ frontend/src/app/pages/user/login/login.component.ts
@@ -4,54 +4,69 @@ import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../../shared/service/user.service';
 import { LoaderComponent } from '../../../component/loader/loader.component';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoaderComponent, FormsModule, RouterLink],
+  imports: [
+    LoaderComponent,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
-  emailUser: string = "";
-  passwordUser: string  = "";
+  emailUser: string = '';
+  passwordUser: string = '';
   statusLoading: boolean = false;
-  ResRequest: string = "";
+  ResRequest: string = '';
 
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.authService.isAdmin()
-    .then(res => {
-      if(res){
-        this.router.navigate(['/home']);
-      }
-    })
-    .catch(err => console.log(err))
+    this.authService
+      .isAdmin()
+      .then((res) => {
+        if (res) {
+          this.router.navigate(['/home']);
+        }
+      })
+      .catch((err) => console.log(err));
   }
 
-
-  onSubmit(event: any){
+  onSubmit(event: any) {
     event.preventDefault();
     this.statusLoading = true;
-    this.ResRequest = ""
-    if ((this.emailUser === "") || (this.passwordUser === undefined)) {
-        this.ResRequest = ""
-        this.ResRequest = "Entrer les informations"
-        this.statusLoading = false;
-        return
-    };
-    this.userService.loginUser(this.emailUser, this.passwordUser)
-      .subscribe(res => {
-        if(res.message === "Utilisateur connecté"){
+    this.ResRequest = '';
+    if (this.emailUser === '' || this.passwordUser === undefined) {
+      this.ResRequest = '';
+      this.ResRequest = 'Entrer les informations';
+      this.statusLoading = false;
+      return;
+    }
+    this.userService
+      .loginUser(this.emailUser, this.passwordUser)
+      .subscribe((res) => {
+        if (res.message === 'Utilisateur connecté') {
           localStorage.setItem('token', res.token);
           localStorage.setItem('user', res.userId);
           this.router.navigate(['/home']);
           window.location.reload();
         }
-        this.ResRequest = res.message
+        this.ResRequest = res.message;
         this.statusLoading = false;
-      })
+      });
   }
-
 }
--- frontend/src/app/shared/service/user.service.ts
+++ frontend/src/app/shared/service/user.service.ts
@@ -3,32 +3,44 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  uri = 'http://localhost:8010/api/utilisateur';
-  token = localStorage.getItem("user") + " " + localStorage.getItem("token");
+  uri = 'http://localhost:3000/api/utilisateur';
+  token = localStorage.getItem('user') + ' ' + localStorage.getItem('token');
   headers = new HttpHeaders({
-    'Authorization': this.token
+    Authorization: this.token,
   });
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // Recuperation de l'utilisateur actif
-  getUserConnected(): Observable<any> { 
-    return this.http.get<any>(this.uri + "/actif", { headers: this.headers });
+  getUserConnected(): Observable<any> {
+    return this.http.get<any>(this.uri + '/actif', { headers: this.headers });
   }
 
   // Connexion de l'utilisateur vers le backend
   loginUser(email: string, password: string): Observable<any> {
-    return this.http.post<any>(this.uri + "/connexion", {email: email, password: password});
+    return this.http.post<any>(this.uri + '/connexion', {
+      email: email,
+      password: password,
+    });
   }
 
   // Inscription d'un utilisateur vers le backend
-  registerUser(nom: string, prenom: string, role: string, email: string, password: string): Observable<any> {
-    return this.http.post<any>(this.uri + "/inscription", {nom: nom, prenom: prenom, role: role, email: email, password: password});
+  registerUser(
+    nom: string,
+    prenom: string,
+    role: string,
+    email: string,
+    password: string
+  ): Observable<any> {
+    return this.http.post<any>(this.uri + '/inscription', {
+      nom: nom,
+      prenom: prenom,
+      role: role,
+      email: email,
+      password: password,
+    });
   }
-
-
-
 }
