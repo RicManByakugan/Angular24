ef766191d36cf58c4b4abe319f09029374d23a2c
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2024-04-28 15:43:01 +0300

    make paginator work

--- frontend/src/app/pages/student/student.component.ts
+++ frontend/src/app/pages/student/student.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Assignment } from '../../interfaces/assignment.interface';
 import { AssignmentCardComponent } from '../assignments/assignment-card/assignment-card.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { AssignmentsService } from '../../shared/service/assignments.service';
 import { MatButtonModule } from '@angular/material/button';
@@ -24,6 +24,11 @@ export class StudentComponent {
   assignments!: Assignment[];
   page = 1;
   pageSize = 12;
+  totalPages!: number;
+  nextPage!: number;
+  prevPage!: number;
+  hasNextPage!: boolean;
+  hasPrevPage!: boolean;
 
   constructor(private assignmentService: AssignmentsService) {}
 
@@ -35,8 +40,18 @@ export class StudentComponent {
     this.assignmentService
       .getAssignmentsPagines(this.page, this.pageSize)
       .subscribe((value) => {
-        console.log(value);
         this.assignments = value.docs;
+        this.totalPages = value.totalPages;
+        this.nextPage = value.nextPage;
+        this.prevPage = value.prevPage;
+        this.hasNextPage = value.hasNextPage;
+        this.hasPrevPage = value.hasPrevPage;
       });
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.page = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.getAssignments();
   }
 }
