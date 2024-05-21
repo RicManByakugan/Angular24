ae26884f536938f80f251624803f50e8dc2a9b0a
Ramananjoelina Tokiniaina Angelo <ramananjoelinatokiniaina@gmail.com>
2024-05-21 21:20:50 +0300

    fix file pipe

--- frontend/src/app/shared/pipes/file-path.pipe.ts
+++ frontend/src/app/shared/pipes/file-path.pipe.ts
@@ -11,6 +11,8 @@ export class FilePathPipe implements PipeTransform {
   // uri = "https://angularmbdsmadagascar2024.onrender.com/";
 
   transform(value: string | undefined): unknown {
-    return value ? this.uri + value.replace(/\\/g, '/') : '';
+    return value
+      ? this.uri.replace('/api', '') + '/' + value.replace(/\\/g, '/')
+      : '';
   }
 }
